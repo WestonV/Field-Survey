f768bb5f9f1ae4da671bd5c5c2329fe14b0e8b27
Weston Verhulst <w2visme@gmail.com>
2018-11-14 10:59:47 -0600

    First commit

---
+++ Field Survey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Field Survey
+//
+//  Created by Weston Verhulst on 11/14/18.
+//  Copyright © 2018 Weston Verhulst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
